f938deae35f6ec307eaf373be5cf9b1af5c6c5ea
kawsar007 <imkawsar007@gmail.com>
2023-06-04 20:12:10 -0700

    initial commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<section class="header">
+
+   <a href="home.php" class="logo">
+      <!-- <img class="img" src="images/logo.jpg" alt="Logo"> -->
+      EVENT MANAGEMENT
+   </a>
+
+   <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="portfolio.php">Portfolio</a>
+      <a href="pricing.php">Pricing</a>
+      <a href="contact.php">Event Booked</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
---
+++ home.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="container">
+
+<?php @include 'header.php'; ?>
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide" style="background:url(images/event-planer.jpg) no-repeat">
+            <div class="content">
+               <h3>The Event Planner</h3>
+               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, obcaecati quos. Minima eius qui id quas atque corporis, quia nulla.</p>
+               <a href="about.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+            <div class="content">
+               <h3>plan your wedding!</h3>
+               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, obcaecati quos. Minima eius qui id quas atque corporis, quia nulla.</p>
+               <a href="about.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/birthday.jpg) no-repeat">
+            <div class="content">
+               <h3>plan your birthday!</h3>
+               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, obcaecati quos. Minima eius qui id quas atque corporis, quia nulla.</p>
+               <a href="about.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+      </div>
+
+      <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+<section class="services">
+
+   <h1 class="heading">our services</h1>
+
+   <div class="swiper service-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <img src="images/photography1.jpeg" alt="">
+            <div class="content">
+               <h3>photography</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, pariatur!</p>
+               <a href="about.php" class="btn">about us</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/service-2.jpg" alt="">
+            <div class="content">
+               <h3>wedding registory</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, pariatur!</p>
+               <a href="about.php" class="btn">about us</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/birthday1.jpeg" alt="">
+            <div class="content">
+               <h3>Birthday Party</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, pariatur!</p>
+               <a href="about.php" class="btn">about us</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/service-4.jpg" alt="">
+            <div class="content">
+               <h3>wedding cake</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, pariatur!</p>
+               <a href="about.php" class="btn">about us</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/service-5.jpg" alt="">
+            <div class="content">
+               <h3>wedding ceremony</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, pariatur!</p>
+               <a href="about.php" class="btn">about us</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/service-6.jpg" alt="">
+            <div class="content">
+               <h3>fine dining</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, pariatur!</p>
+               <a href="about.php" class="btn">about us</a>
+            </div>
+         </div>
+
+      </div>
+
+      <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="heading">happy clients</h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <img src="images/pic-1.png" alt="">
+            <h3>john doe</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga magni perspiciatis aut repellat nihil? Eveniet fugit eius quos architecto repellendus.</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/pic-2.png" alt="">
+            <h3>Alex Bell</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga magni perspiciatis aut repellat nihil? Eveniet fugit eius quos architecto repellendus.</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/pic-3.png" alt="">
+            <h3>Adam Reo</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga magni perspiciatis aut repellat nihil? Eveniet fugit eius quos architecto repellendus.</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/pic-4.png" alt="">
+            <h3>john doe</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga magni perspiciatis aut repellat nihil? Eveniet fugit eius quos architecto repellendus.</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/pic-5.png" alt="">
+            <h3>john doe</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga magni perspiciatis aut repellat nihil? Eveniet fugit eius quos architecto repellendus.</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <img src="images/pic-6.png" alt="">
+            <h3>john doe</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga magni perspiciatis aut repellat nihil? Eveniet fugit eius quos architecto repellendus.</p>
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+         </div>
+
+      </div>
+
+      <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+<?php @include 'footer.php'; ?>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
